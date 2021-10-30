b6d16d0d3290d4ba9e917d1279c6191fb94026b1
GoldenHuntingBear <beringer.lam@gmail.com>
2021-10-30 12:35:35 +0200

    More data and current milestone added

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -7,11 +7,13 @@ import data from "./testData";
 import Spinner from '@momentum-ui/react/lib/Spinner';
 
 const HomePage = (props) => {
-  var currentMilestone;
+  var currentMilestone = props.data["team"]["milestones"].find(milestone => (milestone["done"]==0));
+
+  var currentProgress = props.data["team"]["tp"];
   props.data["team"]["milestones"].forEach(milestone => {
-    if (milestone["done"]==0) {
-      currentMilestone=milestone
-    }    
+    if (milestone["done"]==1) {
+      currentProgress -= milestone["tpNeeded"]
+    }
   });
 
   return (
@@ -35,7 +37,7 @@ const HomePage = (props) => {
           progressSize={24}
           trackColor="#eeeeee"
           trackSize={24}
-          dataIndex={100}
+          dataIndex={currentProgress/currentMilestone["tpNeeded"]*360}
           style={{width: 280}}
         />
         <p>Reward for the current milestone: {currentMilestone["reward"]}</p>
