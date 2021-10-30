c37da875311318a5e23c3602ed958c96e94ff311
GoldenHuntingBear <beringer.lam@gmail.com>
2021-10-30 16:22:04 +0200

    kill monsters

--- src/components/Home/todo.js
+++ src/components/Home/todo.js
@@ -4,7 +4,17 @@ import monster from './monster.png';
 
 export default function ToDo(props) {
   const killMonster = (button) => {
-    button.target.style.transform = "rotate(-90deg)";
+    var className = button.target.className;
+    if(className.includes("monster_dead")) {
+      console.log(className);
+      className.replace("monster_dead", "");
+      console.log(className);
+    }
+    else {
+      className += " monster_dead";
+    }
+    button.target.className = className;
+    // button.target.style.transform = "rotate(-90deg)";
   };
   return (
       <div className="container sword" style={{marginTop: "3rem", padding: "2rem"}}>
@@ -12,7 +22,7 @@ export default function ToDo(props) {
                   (
                     <div className="container" key={task.id} style={{width: 350, height: 60, display: "flex", alignItems: "center", borderBottom: "1px solid gray", paddingTop: "0.5rem"}}>
                       <button style={{ background: "none", border: "none"}} onClick={killMonster.bind(this)}>
-                        <img className="sword" src={monster} alt="monster" style={{ width: 30, height: 30}}/>
+                        <img className="sword monster_alive" src={monster} alt="monster"/>
                       </button>
                       <p>{task.name}</p>
                     </div>      
