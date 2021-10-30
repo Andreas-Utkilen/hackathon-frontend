d516b2e1e853c2e0bd03d7940ece7a4b3e5ec2bb
Andreas Traa Utkilen <andreastraautkilen@gmail.com>
2021-10-30 15:38:08 +0200

    kill monsters

--- src/components/Home/todo.js
+++ src/components/Home/todo.js
@@ -1,14 +1,19 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { Checkbox, CheckboxGroup } from '@momentum-ui/react';
 import monster from './monster.png';
+
 export default function ToDo(props) {
+  const killMonster = (button) => {
+    button.target.style.transform = "rotate(-90deg)";
+  };
   return (
-      <div className="container" style={{marginTop: "3rem"}}>
+      <div className="container sword" style={{marginTop: "3rem", padding: "2rem"}}>
               {props.data.tasks.map((task) =>
                   (
                     <div className="container" key={task.id} style={{width: 350, height: 60, display: "flex", alignItems: "center", borderBottom: "1px solid gray", paddingTop: "0.5rem"}}>
-                      <img src={monster} alt="monster" style={{ width: 30, height: 30}}/>
+                      <button style={{ background: "none", border: "none"}} onClick={killMonster.bind(this)}>
+                        <img className="sword" src={monster} alt="monster" style={{ width: 30, height: 30}}/>
+                      </button>
                       <p>{task.name}</p>
                     </div>      
                   )
